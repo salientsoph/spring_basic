000a002a81ef01fed726eb1027a13b09e7937dda
salientsoph <salientsoph@gmail.com>
2023-09-05 18:53:12 +0900

    java to spring

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -6,14 +6,23 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static Logger log = (Logger) LoggerFactory.getLogger(MemberApp.class.getClass());
     public static void main(String[] args) {
 
-        MemberService memberService = new MemberServiceImpl();
+
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
+
         Member findMember = memberService.findMember(1L);
         log.info("new member : " + memberA);
         log.info("FindMember : " + findMember);
---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,4 @@
+package hello.core.beanfind;
+
+class ApplicationContextBasicFindTest {
+}
