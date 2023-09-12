dd328694405640e68604b7f7f36377c038051315
salientsoph <salientsoph@gmail.com>
2023-09-12 22:34:38 +0900

    scope

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,7 +1,9 @@
 package hello.core.scope;
 
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -26,21 +28,32 @@ public class SingletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;  //ClientBean 생성 시점에 주입
+
+        //private final PrototypeBean prototypeBean;  //ClientBean 생성 시점에 주입
 
         @Autowired
+        //private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        //private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        /*
+        @Autowired
+
         public ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
         }
+        */
 
         public int logic() {
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
     }
 
-    @Test
+    /*@Test
     void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class, ClientBean.class);
 
@@ -51,7 +64,7 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
         Assertions.assertThat(count2).isEqualTo(2);
-    }
+    }*/
 
     @Scope("prototype")
     static class PrototypeBean {
